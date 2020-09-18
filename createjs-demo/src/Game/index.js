708ab2ecb6e377f64cc45ea2ee664eeaf9822208
Khkh8 <henri.vatto@gmail.com>
2020-09-18 11:42:09 +0300

    testing drawing to canvas

--- createjs-demo/src/Game/index.js
+++ createjs-demo/src/Game/index.js
@@ -6,7 +6,8 @@ import Constants from "../constants/commonConstants";
 
 class Game extends Component {
     state = {
-        scaleFactor: 1
+        scaleFactor: 1,
+        missiles: 10
     }
 
     constructor(props) {
@@ -19,12 +20,20 @@ class Game extends Component {
                 this.setState({ ...this.state, ...{ scaleFactor: Constants.scaleFactor() } });
             }
         });
+
+        var canvas = document.getElementById("testicanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.moveTo(0, 20);
+        ctx.lineTo(200, 20);
+        ctx.font = "10px Arial";
+        ctx.fillText(`Health left: ${Constants.maxHP} Missiles remaining: ${this.state.missiles}`, 10, 16);
+        ctx.stroke();
     }
 
     render() {
         return (
             <div className="Game" style={{ maxWidth: Constants.canvasMaxWidth}}>
-                <canvas style={{ width: Constants.canvasMaxWidth, transform: `scale(${this.state.scaleFactor})` }} />
+                <canvas id="testicanvas" style={{ width: Constants.canvasMaxWidth, transform: `scale(${this.state.scaleFactor})` }} />
             </div>
         );
     }
