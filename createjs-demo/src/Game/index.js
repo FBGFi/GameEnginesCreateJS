d93e4eb99b889744baa3959092659654de5c58d7
FBGFi <aleksi.alhola@gmail.com>
2020-09-18 09:03:01 +0300

    time to branch things up

--- createjs-demo/src/Game/index.js
+++ createjs-demo/src/Game/index.js
@@ -1,30 +1,31 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import './Game.css';
 
 import Constants from "../constants/commonConstants";
 
-class Game extends Component{
+class Game extends Component {
     state = {
         scaleFactor: 1
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    componentDidMount(){       
+    componentDidMount() {
         window.addEventListener('resize', () => {
-            if(window.innerWidth < Constants.canvasMaxWidth){ 
-            this.setState({ ...this.state, ...{ scaleFactor: Constants.scaleFactor() } });
+            if (window.innerWidth < Constants.canvasMaxWidth || this.state.scaleFactor !== 1) {
+                console.log("asd");
+                this.setState({ ...this.state, ...{ scaleFactor: Constants.scaleFactor() } });
             }
         });
     }
 
-    render(){
-        return(
-            <div className="Game">
-                <canvas style={{width: Constants.canvasMaxWidth, transform: `scale(${this.state.scaleFactor})`}} />
+    render() {
+        return (
+            <div className="Game" style={{ width: Constants.canvasMaxWidth*this.state.scaleFactor}}>
+                <canvas style={{ width: Constants.canvasMaxWidth, transform: `scale(${this.state.scaleFactor})` }} />
             </div>
         );
     }
