3847ed80832acfd7dd41cbeaf24ba7eeeab44407
Sami Aartolahti <s.j.aartolahti@gmail.com>
2020-09-25 16:10:05 +0300

    enemy movement and other functions created

--- createjs-demo/src/Game/controllers/EnemyController.js
+++ createjs-demo/src/Game/controllers/EnemyController.js
@@ -14,8 +14,12 @@ export class EnemyController {
     constructor(stage){
         this.stage = stage;
         this.enemies = [];
-        this.spawnEnemies(2);
-        this.handleEnemyMovement();
+        this.spawnEnemies(1);
+        // this.handleEnemyMovement();
+
+        // createjs.Ticker.setFPS(Constants.FPS);
+        createjs.Ticker.addEventListener("tick", this.handleTick);
+        // createjs.Sound.setVolume(0.5);
     }
 
     /**
@@ -33,17 +37,23 @@ export class EnemyController {
      * @author Sami - The enemy takes damage for the amount of damage variable
      * @param {Number} damage 
      */
-    takeDamage = (damage) => {
-        let newHP = this.state.currentHP -= damage;
+    takeDamage = (enemy, damage) => {
+        let newHP = enemy.hp = enemy.hp - damage;
         if (newHP <= 0) {
-            return this.destructor;
+            this.destructor(enemy);
         }
+    }
+
+    removeEnemy = (enemy, i) => {
+        this.stage.removeChild(enemy);
+        this.enemies.splice(i, 1);
     }
 
     /**
      * @author Sami - The enemy is destroyed and it creates a random number while doing so
      */
-    destructor() {
+    destructor(enemy) {
+        this.removeEnemy(enemy);
         return Math.random() < 0.1 ? true : false;
     }
 
@@ -51,14 +61,14 @@ export class EnemyController {
     spawnEnemies = (n) => {
         setInterval(() => {
             for(let i = 0; i < n; i++) {
-                let enemy = this.createEnemy()
+                let enemy = this.createEnemy(2)
                 this.enemies.push(enemy);
                 this.stage.addChild(enemy);
             }
         }, 2000);
     }
 
-    createEnemy = () => {
+    createEnemy = (hp) => {
         let r = Math.random();
         let enemy;
 
@@ -73,8 +83,9 @@ export class EnemyController {
         }
 
         enemy.scale = Constants.playerScale;
-        enemy.x = Constants.canvasMaxWidth * 0.9;
+        enemy.x = Constants.canvasMaxWidth;
         enemy.y = Constants.canvasMaxHeight * (Math.random(Math.floor(Math.random) * 10) * 0.9 + 0.05);
+        enemy.hp = hp;
         // enemy.x = 0;
         // enemy.y = 0; 
 
@@ -86,11 +97,17 @@ export class EnemyController {
     }
 
     handleEnemyMovement = () => {
-        setInterval(() => {
-            for (let i = 0; i < this.enemies.length; i++) {
-                this.move(this.enemies[i], 10, 0);
-                console.log(this.enemies[i].x);
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.move(this.enemies[i], 5, 0);
+            if (this.enemies[i].x === 0 - Constants.playerHeight) {
+                this.removeEnemy(this.enemies[i], i);
             }
-        }, 50);
+        }
+        
+        console.log(this.enemies.length);
+    }
+
+    handleTick = (event) => {
+        this.handleEnemyMovement();
     }
 }
